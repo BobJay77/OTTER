186c0da9b800106e793a793dd8f6b75d2c16be9a
Farhan <farhanirani77@gmail.com>
2020-09-25 18:29:20 -0400

    added 2nd frag shader

--- samples/INFR-1350U/Week3-Starter/src/main.cpp
+++ samples/INFR-1350U/Week3-Starter/src/main.cpp
@@ -121,6 +121,30 @@ int main() {
 	 BufferAttribute(1, 3, GL_FLOAT, false, 0, NULL )
 		});
 
+	static const float interleaved[] = {
+		// X     Y     Z      R     G     B
+		 0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 0.0f,
+		 0.5f, 0.5f, 0.5f, 0.3f, 0.2f, 0.5f,
+	    -0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 0.0f,
+		 0.75f, 1.0f, 0.5f, 1.0f, 1.0f, 1.0f
+	};
+	VertexBuffer* interleaved_vbo = new VertexBuffer();
+	interleaved_vbo->LoadData(interleaved, 6 * 4);
+
+		static const uint16_t indices[] = {
+			0, 1, 2,
+			 1, 3, 2
+	};
+	IndexBuffer* interleaved_ibo = new IndexBuffer();
+	interleaved_ibo->LoadData(indices, 3 * 2);
+
+	size_t stride = sizeof(float) * 6;
+	VertexArrayObject* vao2 = new VertexArrayObject();
+	vao2->AddVertexBuffer(interleaved_vbo, {
+	BufferAttribute(0, 3, GL_FLOAT, false, stride, 0),
+	BufferAttribute(1, 3, GL_FLOAT, false, stride, sizeof(float) * 3),
+		});
+	vao2->SetIndexBuffer(interleaved_ibo);
 
 	// Load our shaders
 
@@ -128,6 +152,11 @@ int main() {
 	shader->LoadShaderPartFromFile("shaders/vertex_shader.glsl", GL_VERTEX_SHADER);
 	shader->LoadShaderPartFromFile("shaders/frag_shader.glsl", GL_FRAGMENT_SHADER);
 	shader->Link();
+
+	Shader* frag = new Shader();
+	frag->LoadShaderPartFromFile("shaders/vertex_shader.glsl", GL_VERTEX_SHADER);
+	frag->LoadShaderPartFromFile("shaders/frag_shader2.glsl", GL_FRAGMENT_SHADER);
+	frag->Link();
 
 	// GL states
 	glEnable(GL_DEPTH_TEST);
@@ -152,10 +181,20 @@ int main() {
 		vao->Bind();
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
+		frag->Bind();
+		vao2->Bind();
+		glDrawElements(GL_TRIANGLES, interleaved_ibo->GetElementCount(), interleaved_ibo->GetElementType(), nullptr);
+		vao2->UnBind();
+		frag->UnBind();
+
 		glfwSwapBuffers(window);
 	}
 
 	delete shader;
+	delete frag;
+	delete vao2;
+	delete interleaved_ibo;
+	delete interleaved_vbo;
 	delete vao;
 	delete posVbo;
 	delete color_vbo;
